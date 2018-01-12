c61943d5e25fa24aae278af1806c514d53683930
chrispschan <chris.ps.chan@pacim.com>
2018-01-12 12:49:17 +0800

    add scrollTo & document

---
+++ src/app/scrollTo/index.js
@@ -0,0 +1,37 @@
+export default function ScrollTo (element, to, duration = 0) {
+    let start = element.scrollTop,
+        change = null,
+        currentTime = 0,
+        increment = 20;
+    
+    let animateScroll = function () {
+        currentTime += increment;
+        let val = Math.easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if (currentTime < duration)
+            setTimeout(animateScroll, increment);
+    };
+
+    if (typeof to === 'number') change = to - start;
+    else if (typeof to === 'string') {
+        let _ele = document.getElementById(to) ? [document.getElementById(to)] : document.getElementsByClassName(to);
+
+        if (_ele.length > 0)
+            change = _ele[0].offsetTop - start;
+    } else if (to.offsetTop) change = to.offsetTop - start;
+    else if (to.length > 0)
+        if (to[0].offsetTop) change = to[0].offsetTop - start;
+    
+    if (change !== null) animateScroll();
+}
+
+// t = current time
+// b = start value
+// c = change in value
+// d = duration
+Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+};
