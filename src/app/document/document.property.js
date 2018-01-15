4a9a1c8b8a6588be6ae6c9328ad25f6e5b15bc66
chrispschan <chris.ps.chan@pacim.com>
2018-01-15 09:34:55 +0800

    document.property.js - add getHTML property to get HTML tag element

--- src/app/document/document.property.js
+++ src/app/document/document.property.js
@@ -5,3 +5,6 @@ document.ready = function (fn) {
     else
         document.addEventListener('DOMContentLoaded', fn);
 };
+
+// get html tag element
+document.getHTML = () => document.getElementsByTagName('HTML').length > 0 ? document.getElementsByTagName('HTML')[0] : null;
