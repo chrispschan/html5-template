bd7ffa11c95aac8dc39dd9c242de522a95dc2718
chrispschan <chris.ps.chan@pacim.com>
2018-01-16 15:41:59 +0800

    document.property.js updated

--- src/app/document/document.property.js
+++ src/app/document/document.property.js
@@ -5,6 +5,3 @@ document.ready = function (fn) {
     else
         document.addEventListener('DOMContentLoaded', fn);
 };
-
-// get html tag element
-document.getHTML = () => document.getElementsByTagName('HTML').length > 0 ? document.getElementsByTagName('HTML')[0] : null;
