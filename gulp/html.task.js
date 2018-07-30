a856f5cb015871ec47a9e750a502571a5f0f17de
chrispschan <chris.ps.chan@accenture.com>
2018-07-30 12:13:10 +0800

    fix - mac file path

--- gulp/html.task.js
+++ gulp/html.task.js
@@ -12,33 +12,33 @@ import manageEnvironment from './../src/manageEnvironment.js';    // nunjucks en
 import gulpOptions from './../gulp.options.js';
 
 const watchFiles = {
-    serve: gulpOptions.server.root,
-    hb: './src/app/**/*.{hbs,handlebars}',    // handlebars partials
-    nunjucks: ['./src/app/'],    // nunjucks partials
-    handlebars: ['./src/html/**/*.handlebars', './src/html/**/*.hbs', '!./src/html/**/_*.handlebars', '!./src/html/**/_*.hbs'],
-    html: ['./src/html/**/*.html', '!./src/html/**/_*.html'],
-    content: ['./src/data/**/*.json', '!./src/data/**/_*.json']    // html content json
-},
-outputFiles = {
-    fonts: watchFiles.serve + gulpOptions.outputFiles.fonts,
-    img: watchFiles.serve + gulpOptions.outputFiles.img,
-    babel: gulpOptions.outputFiles.babel,
-    js: watchFiles.serve + gulpOptions.outputFiles.js,
-    scss: watchFiles.serve + gulpOptions.outputFiles.scss,
-    html: watchFiles.serve + gulpOptions.outputFiles.html
-};
+        serve: gulpOptions.server.root,
+        hb: './src/app/**/*.{hbs,handlebars}',    // handlebars partials
+        nunjucks: ['./src/app/'],    // nunjucks partials
+        handlebars: ['./src/html/**/*.handlebars', './src/html/**/*.hbs', '!./src/html/**/_*.handlebars', '!./src/html/**/_*.hbs'],
+        html: ['./src/html/**/*.html', '!./src/html/**/_*.html'],
+        content: ['./src/data/**/*.json', '!./src/data/**/_*.json']    // html content json
+    },
+    outputFiles = {
+        fonts: watchFiles.serve + gulpOptions.outputFiles.fonts,
+        img: watchFiles.serve + gulpOptions.outputFiles.img,
+        babel: gulpOptions.outputFiles.babel,
+        js: watchFiles.serve + gulpOptions.outputFiles.js,
+        scss: watchFiles.serve + gulpOptions.outputFiles.scss,
+        html: watchFiles.serve + gulpOptions.outputFiles.html
+    };
 
 let contentPath = watchFiles.content,
-contentData = {
-    path: {
-        fonts: outputFiles.fonts[outputFiles.fonts - 1] == '/' ? outputFiles.fonts.replace(watchFiles.serve, '/') : outputFiles.fonts.replace(watchFiles.serve, '/') + '/',
-        img: outputFiles.img[outputFiles.img - 1] == '/' ? outputFiles.img.replace(watchFiles.serve, '/') : outputFiles.img.replace(watchFiles.serve, '/') + '/',
-        js: outputFiles.js[outputFiles.js - 1] == '/' ? outputFiles.js.replace(watchFiles.serve, '/') : outputFiles.js.replace(watchFiles.serve, '/') + '/',
-        babel:outputFiles.babel,
-        css: outputFiles.scss[outputFiles.scss - 1] == '/' ? outputFiles.scss.replace(watchFiles.serve, '/') : outputFiles.scss.replace(watchFiles.serve, '/') + '/',
-        html: outputFiles.html[outputFiles.html - 1] == '/' ? outputFiles.html.replace(watchFiles.serve, '/') : outputFiles.html.replace(watchFiles.serve, '/') + '/'
-    }
-};
+    contentData = {
+        path: {
+            fonts: outputFiles.fonts[outputFiles.fonts - 1] == '/' ? outputFiles.fonts.replace(watchFiles.serve, '/') : outputFiles.fonts.replace(watchFiles.serve, '/') + '/',
+            img: outputFiles.img[outputFiles.img - 1] == '/' ? outputFiles.img.replace(watchFiles.serve, '/') : outputFiles.img.replace(watchFiles.serve, '/') + '/',
+            js: outputFiles.js[outputFiles.js - 1] == '/' ? outputFiles.js.replace(watchFiles.serve, '/') : outputFiles.js.replace(watchFiles.serve, '/') + '/',
+            babel: outputFiles.babel,
+            css: outputFiles.scss[outputFiles.scss - 1] == '/' ? outputFiles.scss.replace(watchFiles.serve, '/') : outputFiles.scss.replace(watchFiles.serve, '/') + '/',
+            html: outputFiles.html[outputFiles.html - 1] == '/' ? outputFiles.html.replace(watchFiles.serve, '/') : outputFiles.html.replace(watchFiles.serve, '/') + '/'
+        }
+    };
 
 // hb build
 gulp.task(
@@ -74,14 +74,14 @@ gulp.task('nunjucks:watch', () => gulp.watch(watchFiles.html, ['nunjucks:build']
 
 // get content json
 gulp.task(
-    'content:get', 
+    'content:get',
     () => gulp.src(contentPath)
-        .pipe(each(function(content, file, callback) {
-            let newContent = '// my comment\n' + content,
-                fileArr = file.path.split('\\'),
+        .pipe(each(function (content, file, callback) {
+            let newContent = content,
+                fileArr = file.path.search(/\\/) !== -1 ? file.path.split('\\') : file.path.split('\/'),
                 val = fileArr[fileArr.length - 1].replace('.json', '');
 
-            fs.readFile(file.path, 'utf8', function(err, data){
+            fs.readFile(file.path, 'utf8', function (err, data) {
                 contentData[val] = JSON.parse(data.toString('utf8').replace(/^\uFEFF/, ''));
                 callback(null, newContent);
             });
@@ -96,10 +96,10 @@ gulp.task(
 
 // content json watch
 gulp.task('content:watch',
-() => {
-    gulp.watch(watchFiles.content, (event) => {
-        contentPath = event.path;
+    () => {
+        gulp.watch(watchFiles.content, (event) => {
+            contentPath = event.path;
 
-        gulp.start('content:get');
+            gulp.start('content:get');
+        });
     });
-});
