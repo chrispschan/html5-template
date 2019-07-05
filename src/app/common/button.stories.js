b3550265683334060412b2548667b97c2c6b9c7d
chrispschan <chris.ps.chan@accenture.com>
2019-07-05 15:38:26 +0800

    add Form stories

--- src/app/common/button.stories.js
+++ src/app/common/button.stories.js
@@ -3,13 +3,13 @@ import { storiesOf } from '@storybook/html';
 
 import withCode from 'storybook-addon-code-improved';
 
-import htmlCode from './inlineTextElements.stories.html';
-import ResetVariablesCode from '!!raw-loader!./reset/link.variables.scss';
+import htmlCode from './button.stories.html';
+import ResetVariablesCode from '!!raw-loader!./reset/button.variables.scss';
 
-storiesOf('Common/Typography', module)
+storiesOf('Common/Form', module)
   .addDecorator(withCode({
     type: 'sass',
     label: 'variables',
     code: ResetVariablesCode
   }))
-  .add('Inline Text Elements', () => htmlCode);
+  .add('Button', () => htmlCode);
--- src/app/common/inputField.stories.js
+++ src/app/common/inputField.stories.js
@@ -3,13 +3,13 @@ import { storiesOf } from '@storybook/html';
 
 import withCode from 'storybook-addon-code-improved';
 
-import htmlCode from './inlineTextElements.stories.html';
-import ResetVariablesCode from '!!raw-loader!./reset/link.variables.scss';
+import htmlCode from './inputField.stories.html';
+import ResetVariablesCode from '!!raw-loader!./reset/input.variables.scss';
 
-storiesOf('Common/Typography', module)
+storiesOf('Common/Form', module)
   .addDecorator(withCode({
     type: 'sass',
     label: 'variables',
     code: ResetVariablesCode
   }))
-  .add('Inline Text Elements', () => htmlCode);
+  .add('Input Field', () => htmlCode);
