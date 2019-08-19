62526cac03b0aa813145ab92579e69518fb3c83d
chrispschan <chris.ps.chan@accenture.com>
2019-08-19 08:21:04 +0800

    fix - Object.deepAssign - date value

--- src/app/object/object.property.js
+++ src/app/object/object.property.js
@@ -8,7 +8,7 @@ Object.deepAssign = function () {
         for (let i = 0; i < objs.length; i++) {
             if (typeof objs[i] === 'object') {
                 for (let key in objs[i]) {
-                    if (typeof objs[i][key] === 'object' && !Array.isArray(objs[i][key]) && objs[i][key].constructor !== RegExp)
+                    if (typeof objs[i][key] === 'object' && !Array.isArray(objs[i][key]) && objs[i][key].constructor !== RegExp && Object.prototype.toString.call(objs[i][key]) !== '[object Date]')
                         _objs[key] = assignObj([_objs[key] ? _objs[key] : {}, objs[i][key]]);
                     else if (Array.isArray(objs[i][key])) {
                         _objs[key] = [];
